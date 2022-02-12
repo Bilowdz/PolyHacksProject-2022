74a70c478b579fc908c9d0e3ecb45aff3dccf8dc
Bilowdz <simonbilodeau27@gmail.com>
2022-02-12 01:52:05 -0500

    Threading added
    To make program usable while timer is going down towards alarm

--- main.py
+++ main.py
@@ -2,65 +2,66 @@ from datetime import datetime
 import time
 # from playsound import playsound
 from tkinter import *
+from threading import *
 
 
 def validate_time(alarm_time):
     if len(alarm_time) != 8:
-        return "Invalid time format! Please try again..."
+        return "Invalid time format! Please try again (HH:MM:SS)."
     else:
         if int(alarm_time[0:2]) > 23:
-            return "Invalid HOUR format! Please try again..."
+            return "Invalid HOUR format! Please try again (HH:MM:SS)."
         elif int(alarm_time[3:5]) > 59:
-            return "Invalid MINUTE format! Please try again..."
+            return "Invalid MINUTE format! Please try again (HH:MM:SS)."
         elif int(alarm_time[6:8]) > 59:
-            return "Invalid SECOND format! Please try again..."
+            return "Invalid SECOND format! Please try again (HH:MM:SS)."
         else:
             return "ok"
 
 
-class GUI:
-
-    def __init__(self):
-        window = Tk()
-        window.title("Alarm Clock")
-        window.geometry('400x400')
-        self.lbl = Label(window, text="Enter time in 'HH:MM:SS' format: ")
-        self.lbl.grid(column=0, row=0)
-        self.alarm_time = Entry(window, width=20)
-        self.alarm_time.grid(column=1, row=0)
-        self.alarm_time.focus()
-        btn = Button(window, text="Click Me", command=self.clicked)
-        btn.grid(column=2, row=0)
-        window.mainloop()
-
-    def clicked(self):
-        while True:
-            alarm = self.alarm_time.get()
-            validate = validate_time(alarm.lower())
-            if validate != "ok":
-                self.lbl.configure(text=validate)
-            else:
-                self.lbl.configure(text=f"Setting alarm for {alarm}...")
-                break
-        alarm_hour = alarm[0:2]
-        alarm_min = alarm[3:5]
-        alarm_sec = alarm[6:8]
-
-        while True:
-            for i in range(5):
-                print(i)
-                time.sleep(1)
-            now = datetime.now()
-            current_hour = now.strftime("%H")
-            current_min = now.strftime("%M")
-            current_sec = now.strftime("%S")
-
-            if alarm_hour == current_hour:
-                if alarm_min == current_min:
-                    if alarm_sec <= current_sec:
-                        self.lbl.configure(text="Wake Up!")
-                        # playsound('laugh.wav')
-                        break
+window = Tk()
+window.title("Alarm Clock")
+window.geometry('400x400')
+lbl = Label(window, text="Enter time in 'HH:MM:SS' format: ")
+lbl.grid(column=0, row=0)
+alarm_time = Entry(window, width=20)
+alarm_time.grid(column=1, row=0)
+alarm_time.focus()
 
 
-gui = GUI()
+def threading():
+    t1 = Thread(target=clicked)
+    t1.start()
+
+
+def clicked():
+    while True:
+        alarm = alarm_time.get()
+        validate = validate_time(alarm.lower())
+        if validate != "ok":
+            lbl.configure(text=validate)
+        else:
+            lbl.configure(text=f"Setting alarm for {alarm}...")
+            break
+    alarm_hour = alarm[0:2]
+    alarm_min = alarm[3:5]
+    alarm_sec = alarm[6:8]
+
+    while True:
+        time.sleep(1)
+        now = datetime.now()
+        current_hour = now.strftime("%H")
+        current_min = now.strftime("%M")
+        current_sec = now.strftime("%S")
+
+        if alarm_hour == current_hour:
+            if alarm_min == current_min:
+                if alarm_sec <= current_sec:
+                    lbl.configure(text="Wake Up!")
+                    # playsound('laugh.wav')
+                    break
+
+
+btn = Button(window, text="Click Me", command=threading)
+btn.grid(column=2, row=0)
+window.mainloop()
